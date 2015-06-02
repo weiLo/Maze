c274bc30664b099989c9a8e4fc9dda631f7f123a
Wei-Hao Lo <whlo3@myseneca.ca>
2015-06-01 23:44:23 -0400

    Maze 1.3
    Print maze layout

--- Maze.py
+++ Maze.py
@@ -3,18 +3,18 @@ from array import array
 
 
 
-class MickeyMove(object):
+class MickeyMove():
+    
     '''
     Constructor with starting position x , y
                      the mouse moved path 
     ''' 
-    def __init__(self, map ,x_start, y_start):
+    def __init__(self, map,x_start , y_start):
         print("create mickey maze")
         self.x_start = x_start
         self.y_start = y_start
         self.path = Queue(maxsize=0)
         self.maze_map = map
-     
     '''
     Return True if next move is available   
     ''' 
@@ -23,43 +23,40 @@ class MickeyMove(object):
         return False
 
     '''
-    
     '''
     @classmethod
     def move(self, x_move, y_move):
-        return True
+       return True
+
+    def setMaze(self,maze):
+        self.maze_map = maze
     
     @classmethod
-    def printMaze(self):
+    def printMaze(self, maze_map):
         print("in maze")
-        for row in self.maze_map:
+        for row in maze_map:
             for val in row:
                 print(val,end=" ")
-            print(end="\n")    
-        
+            print(end="\n")
 
+    
 
+def main():
+    maze =[
+           [2, 2, 2, 2, 2, 2, 2],
+           [2, 0, 0, 0, 0, 0, 2],
+           [2, 0, 2, 0, 2, 0, 2],
+           [2, 0, 0, 0, 0, 2, 2],
+           [2, 2, 0, 2, 0, 2, 2],
+           [2, 0, 0, 0, 0, 0, 2],
+           [2, 2, 2, 2, 2, 2, 2]]
 
-
-
-
-
-maze =[
-        [2, 2, 2, 2, 2, 2, 2],
-        [2, 0, 0, 0, 0, 0, 2],
-        [2, 0, 2, 0, 2, 0, 2],
-        [2, 0, 0, 0, 0, 2, 2],
-        [2, 2, 0, 2, 0, 2, 2],
-        [2, 0, 0, 0, 0, 0, 2],
-        [2, 2, 2, 2, 2, 2, 2]]
-
-m = MickeyMove(maze, 1,1)
-#try to print maze by using method
-m.printMaze()
-for row in maze:
-    for val in row:
-        print(val,end=" ")
-    print(end="\n")    
+    m = MickeyMove(maze, 1,1)
+    print(m.maze_map)
+    m.printMaze(m.maze_map)
+    
+if __name__ == "__main__":
+    main()
 
 #si = 1
 #sj = 1
